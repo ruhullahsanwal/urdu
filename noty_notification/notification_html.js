755dca6915ff33dc719f211ce03c1c510198e096
ruhullahsanwal <ruhullahsanwal@gmail.com>
2016-08-29 10:31:44 +0500

    Add files via upload

---
+++ noty_notification/notification_html.js
@@ -0,0 +1,3 @@
+// notification body's can be any html string or just string
+var notification_html = [];
+    notification_html[2] = '<div class="activity-item"> <i class="fa fa-heart text-danger"></i> <div class="activity"><iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fflixpanel&width=450&layout=standard&action=like&size=small&show_faces=false&share=false&height=35&appId=1604676803164631" width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>Like us on Facebook</div>';
